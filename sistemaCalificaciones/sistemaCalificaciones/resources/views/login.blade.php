9986c22c3b7bca0c63b09740643aa31612e3c956
ValeriaChavezG <A328937@alumnos.uaslp.mx>
2024-04-17 20:11:56 -0600

    login

---
+++ sistemaCalificaciones/sistemaCalificaciones/resources/views/login.blade.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>login</title>
+
+   <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+     <!-- Scripts -->
+<link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+    integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer"
+/>
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> --}}
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <!-- Scripts -->
+    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
+</head>
+<body>
+    <main class="container align-center p-5">
+        <form method="POST" action="{{route('inicia-sesion')}}">
+            @csrf
+            <div class=mb-3>
+                <label for="emailInput" class="form-label">Email</label>
+                <input type="email" class="form-control" id="emailInput"
+                name="email">
+            </div>
+            <div class=mb-3>
+                <label for="passwordInput" class="form-label">Password</label>
+                <input type="password" class="form-control" id="passwordInput"
+                name="password" required>
+            </div>
+            <div class=mb-3>
+                <input type="checkbox" class="form-check-input" id="rememberCheck" name="remember">
+                <label class="form-check-label" for="rememberCheck">Mantener sesión iniciada</label>
+            </div>
+            <div>
+                <p>¿Np tienes una cuenta? <a href="{{route('registro')}}">Registrate</a></p>
+            </div>
+            <button type="submit" class="btn btn-primary">Acceder</button>
+
+        </form>
+    </main>
+</body>
+</html>
